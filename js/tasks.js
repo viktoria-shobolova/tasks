743dcaca60e0ddf76350e0071cd37825690762c3
Виктория <viktoria.servetnik@yandex.ru>
2021-04-28 14:13:12 +0300

    Дополнения для себя

--- js/tasks.js
+++ js/tasks.js
@@ -1,14 +1,14 @@
 // Задача 67.
 // Сделайте функцию inArray, которая определяет, есть в массиве элемент с заданным текстом или нет. Функция первым параметром должна принимать текст элемента, а вторым - массив, в котором делается поиск. Функция должна возвращать true или false.
 
-function inArray(text, array) {
-	for ( let i = 0; i < array.length; i++) {
-	  if (array[i] === text) 
-	  return true;
-	}
-	return false;
-  }
-  alert (inArray('!', ['Привет', '!', 'май']));
+// function inArray(text, array) {
+// 	for ( let i = 0; i < array.length; i++) {
+// 	  if (array[i] === text) 
+// 	  return true;
+// 	}
+// 	return false;
+//   }
+//   alert (inArray('!', ['Привет', '!', 'май']));
 
 
   
@@ -17,15 +17,31 @@ function inArray(text, array) {
 // Задача 72.
 // Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
 
-mas = [-2,-4,5,7,9,11];
-let vision = mas.reduce(function (sum, elem, _index) {
-	if (sum > 10) {
-		alert(_index);
-		return;
-	} 
-	else {
-	_index++;
+// mas = [-2,-4,5,7,9,11];
+// let vision = mas.reduce(function (sum, elem, _index) {
+// 	if (sum > 10) {
+// 		alert(_index);
+// 		return;
+// 	} 
+// 	else {
+// 	_index++;
 
-		 	return sum + elem;
-			}
-}); 
+// 		 	return sum + elem;
+// 			}
+// }); 
+   
+
+// Задача с факториалом под вопросом
+
+ 
+// function factorial(n) {
+// 	let result = 1;
+// 	while (n) {
+// 	result *= n--
+// 	}
+// 	return result;
+// }
+//  alert(factorial(5));
+
+
+
